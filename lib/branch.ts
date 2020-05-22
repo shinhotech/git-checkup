38d72b5fcc3599d828aa0957017cf57f075fb88c
yanyongjie_18120001 <yanyongjie@shinho.net.cn>
2020-05-22 14:30:52 +0800

    feat: 添加获取branches,tag方法

--- lib/branch.ts
+++ lib/branch.ts
@@ -2,8 +2,7 @@
 const { request } = require('./request')
 
 export const fetch = async function () {
-    const { data: projects } = await request('/projects')
-    // console.log('[data]', projects)
+    const { data: projects } = await request('/projects?visibility=private')
     const items = projects.map((item: { name: any; description: any; id: any }) => {
         return {
             name: item.name,
@@ -12,21 +11,44 @@ export const fetch = async function () {
         }
     })
     // console.log('[items]', JSON.stringify({items}))
-    console.log('[items]', items)
+    // console.log('[items]', items)
     if (items.length) {
         console.log('结果数量',items.length)
         items.forEach (async(o: any) => {
-            console.log(o.id, '[id]')
-            // try {
-            //     const tags = await request({
-            //         method: 'GET',
-            //         url: `/projects/:1838/repository/tags`
-            //         // url: `/projects/:${o.id}/repository/tags`
-            //     })
-            //     console.log('[tags]', tags)
-            // } catch (e) {
-            //     console.log(e, 'e')
-            // }
+            /** 获取分支 */
+            try {
+                const { data: branches } = await request({
+                    method: 'GET',
+                    url: `/projects/${o.id}/repository/branches`
+                })
+                // console.log('[branches]', branches)
+                const branchesList = branches.map((item: { name: any }) => {
+                    return {
+                        projectName: o.name,
+                        branchesName: item.name
+                    }
+                })
+                console.log('[branchesList]', branchesList)
+            } catch (e) {
+                console.log(e, 'e')
+            }
+            /** 获取tag */
+            try {
+                const { data: tags} = await request({
+                    method: 'GET',
+                    url: `/projects/${o.id}/repository/tags`
+                })
+                // console.log('[tags]', tags) 
+                const tagList = tags.map((item: { name: any }) => {
+                    return {
+                        projectName: o.name,
+                        tagName: item.name
+                    }
+                })
+                console.log('[tagList]', tagList) 
+            } catch (e) {
+                console.log(e, 'e')
+            }
         })
     }
 }
--- schedule/branch.ts
+++ schedule/branch.ts
@@ -3,9 +3,10 @@ export class Schedule {
   static schedule = require('node-schedule')
 
   static scheduleCronstyle = () => {
+    fetch ()
     Schedule.schedule.scheduleJob('30 * * * * *', () => {
-      console.log('scheduleCronstyle:' + new Date());
-      fetch ()
+      // console.log('scheduleCronstyle:' + new Date());
+      // fetch ()
     })
   }
 }
