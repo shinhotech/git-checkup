2f10876bb1c523cabdb9c3af680c3a9bf1c4090d
yanyongjie_18120001 <yanyongjie@shinho.net.cn>
2020-05-25 15:32:34 +0800

    feat: 调用发送短信通知功能

--- schedule/branch.ts
+++ schedule/branch.ts
@@ -1,12 +1,17 @@
 import { fetch } from '../lib/branch'
+const sendSMS = require('@shinho-sh/sms')
 export class Schedule {
   static schedule = require('node-schedule')
 
   static scheduleCronstyle = () => {
-    fetch ()
+    // fetch ()
+    // sendSMS('18516236270', '发送test', 'ir')
+
+    
     Schedule.schedule.scheduleJob('30 * * * * *', () => {
       // console.log('scheduleCronstyle:' + new Date());
       // fetch ()
+
     })
   }
 }
