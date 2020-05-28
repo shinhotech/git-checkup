dd0d5e7eb941c6dea50ec904eb7aaab642204e31
sunmengjie_17060036 <sunmengjie@shinho.net.cn>
2020-05-28 17:28:39 +0800

    feat: 正则抽离至配置文件

--- lib/regex.ts
+++ lib/regex.ts
@@ -1,3 +1,7 @@
+import YAML from 'yamljs'
+const config = YAML.load('./config.yaml')
+const regex = config.regex
+
 /**
  * 分支检测
  * @param name 分支名
@@ -7,39 +11,38 @@ export const regexBranch = function (name: string) {
      * master
      */
     const masterRegex = () => {
-        const regex = /^master$/
-        // console.log('masterRegex: ' + regex.test(name))
-        return regex.test(name)
+        return new RegExp(regex.master).test(name)
     }
     /**
      * release
      * 预发布时间：格式为 2019-05-20，例如：分支名 release_2019-05-20
      */
     const releaseRegex = () => {
-        const regex = /^release_\d{4}-\d{2}-\d{2}$/
-        // console.log('releaseRegex: ' + regex.test(name))
-        return regex.test(name)
+        return new RegExp(regex.release).test(name)
     }
     /**
      * feature
      * 功能点名称：格式为小写连字符 -，例如：用户详情 feature_user-details
      */
     const featureRegex = () => {
-        const regex = /^feature_([a-z]{1,}-){0,}[a-z]{1,}$/
-        // console.log('featureRegex: ' + regex.test(name))
-        return regex.test(name)
+        return new RegExp(regex.feature).test(name)
     }
     /**
      * hotfix
      * bug号：为修复对应的 Jira   Bug编号。
      */
     const hotFixRegex = () => {
-        const regex = /^hotfix_\w{1,}-\w{1,}$/
-        // console.log('hotFixRegex: ' + regex.test(name))
-        return regex.test(name)
+        return new RegExp(regex.hotfix).test(name)
+    }
+    /**
+     * dev
+     * 图像项目特殊分支 dev_integrate
+     */
+    const devRegex = () => {
+        return new RegExp(regex.dev).test(name)
     }
 
-    return masterRegex() || releaseRegex() || featureRegex() || hotFixRegex()
+    return masterRegex() || releaseRegex() || featureRegex() || hotFixRegex() || devRegex()
 }
 /**
  * tag检测
@@ -47,7 +50,5 @@ export const regexBranch = function (name: string) {
  * @param name tag名
  */
 export const regexTag = function (name: string) {
-    const regex = /^v(\d{1,}\.){1,}\d$/
-    // console.log('regexTag: ' + regex.test(name))
-    return regex.test(name)
+    return new RegExp(regex.tag).test(name)
 }
